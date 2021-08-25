588f9304ae7d04503963f267984fc69c59d896f7
sandrabosk <AB@MacBook-Pro.local>
2021-08-25 08:27:11 +0200

    add missing useContext, resolve conflicts

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./../context/auth.context";
