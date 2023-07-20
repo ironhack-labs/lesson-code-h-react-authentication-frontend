a10ac752462ef01d4c9fc2c0b73fc01b0ce45c39
Andy Cockroft <acockroft1994@gmail.com>
2023-07-20 18:44:55 +0200

    api response sent to console, rendering not complete

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,34 +1,54 @@
 import * as React from "react";
 import { Button, Input } from "@fluentui/react-components";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function HomePage() {
-  const [value, setValue] = useState("");
-  const [message, setMessage] = useState(null);
+  const [prompt, setPrompt] = useState("");
+  const [message, setMessage] = useState("");
 
-  const getMessages = async () => {
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        message: value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    try {
-      const response = await fetch(
-        "http://localhost:5005/api/completions",
-        options
-      ); console.log("hello");
-      const data = await response.json();
-      console.log(response);
-      console.log(data);
-      setMessage(data.choices[0].message);
-    } catch (error) {
-      console.error(error);
-    }
+  const handlePostReq = () => {
+    const url = "http://localhost:5005/api/completions";
+    const storedToken = localStorage.getItem("authToken");
+    axios
+      .post(
+        url,
+        {
+          message: prompt,
+        },
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        }
+      )
+      .then((resp) => console.log(resp))
+      .catch((error) => console.log(error));
   };
+
+  // const getMessages = async () => {
+  //   const options = {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       message: prompt,
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
+  //   try {
+  //     const response = await fetch(
+  //       "http://localhost:5005/api/completions",
+  //       options
+  //     );
+  //     console.log("hello");
+  //     const data = await response.json();
+  //     console.log(response);
+  //     console.log(data);
+  //     setMessage(data.choices[0].message);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
   return (
     <div>
       <div>
@@ -42,10 +62,10 @@ function HomePage() {
       </div>
       <div>
         <Input
-          onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setPrompt(e.target.value)}
           placeholder="Add some detail"
         />
-        <Button onClick={getMessages}>Let's Book!</Button>
+        <Button onClick={handlePostReq}>Let's Book!</Button>
       </div>
     </div>
   );
