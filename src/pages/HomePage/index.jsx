da1fdd8a89353c825e8ce0dd14d61993afe5d4c3
Andy Cockroft <acockroft1994@gmail.com>
2023-07-20 16:52:01 +0200

    stuck on authorization

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,34 @@
 import * as React from "react";
 import { Button, Input } from "@fluentui/react-components";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5005/api/completions",
+        options
+      ); console.log("hello");
+      const data = await response.json();
+      console.log(response);
+      console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       <div>
@@ -14,8 +41,11 @@ function HomePage() {
         </div>
       </div>
       <div>
-        <Input placeholder="Add some detail" />
-        <Button>Let's Book!</Button>
+        <Input
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add some detail"
+        />
+        <Button onClick={getMessages}>Let's Book!</Button>
       </div>
     </div>
   );
