a5aad471be41a81933ebc000e4fae11241a7d71a
Devin Fiachra <devinduffymeade@gmail.com>
2023-07-23 18:16:56 +0200

    update footer css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import { ReactSVG } from "react-svg";
-import { Text } from "@fluentui/react-components";
+import { Link } from "react-router-dom";
 import sun from "../../assets/icons/dark/sun.svg";
+import home from "../../assets/icons/dark/home.svg";
 import discover from "../../assets/icons/dark/discover.svg";
 import profile from "../../assets/icons/dark/profile.svg";
 import chat from "../../assets/icons/dark/chat.svg";
@@ -10,26 +11,30 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <div>
+      <div className="footer-nav">
         <ReactSVG src={sun} alt="mood" />
+        <div>Home</div>
       </div>
 
-      <div>
+      <div className="footer-nav">
         <ReactSVG src={discover} alt="navigate to our exercises" />
+        <div>Discover</div>
       </div>
 
-      <div>
+      <div className="footer-nav">
         <div className="rectangle-menu">
           <ReactSVG src={plus} alt="navigate to our exercises" />
         </div>
       </div>
 
-      <div>
+      <div className="footer-nav">
         <ReactSVG src={profile} alt="user profile" />
+        <div>Profile</div>
       </div>
 
-      <div>
+      <div className="footer-nav">
         <ReactSVG src={chat} alt="ai chatbot" />
+        <div>Talk</div>
       </div>
     </div>
   );
