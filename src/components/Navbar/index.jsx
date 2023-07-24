06394dfc7bec00c2005d17b819f968d7806005fe
Devin Fiachra <devinduffymeade@gmail.com>
2023-07-24 17:39:17 +0200

    add basic nav for logout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,9 @@ import { Button } from "@fluentui/react-components";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
+import logout from "../../assets/icons/light/logout.png";
+import "./Navbar.css";
+import "../../App.css";
 
 function Navbar() {
   // Subscribe to the AuthContext to gain access to
@@ -9,14 +12,16 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
+    <nav className="navbar">
       {isLoggedIn && (
-        <>
-          <Button onClick={logOutUser}>
-            <Link to="/">Logout</Link>
-          </Button>
-          <span>{user && user.name}'s hopeme Dashboard</span>
-        </>
+        <div className="navbar-1">
+          <div> {user.name}'s hopeme Dashboard</div>
+          <div onClick={logOutUser} className="button-logout">
+            <Link to="/">
+              <img src={logout} />
+            </Link>
+          </div>
+        </div>
       )}
     </nav>
   );
