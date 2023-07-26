9aaff0d11b979d10c47419aedf128ad2c16540bd
Devin Fiachra <devinduffymeade@gmail.com>
2023-07-26 19:29:56 +0200

    basic styles for articles page

--- src/pages/ArticlesPage/index.jsx
+++ src/pages/ArticlesPage/index.jsx
@@ -3,12 +3,76 @@ import Footer from "../../components/Footer";
 import "../../App.css";
 import "./ArticlesPage.css";
 import luna from "../../assets/luna/welcome.png";
+import anxiety from "../../assets/luna/concern.png";
+import depression from "../../assets/luna/despair.png";
+import love from "../../assets/luna/love.png";
+import running from "../../assets/luna/determined.png";
+import mindfulness from "../../assets/luna/meditate.png";
+import recovery from "../../assets/luna/aokay.png";
 
 function ArticlesPage() {
   return (
     <div class="container">
-      <div className="main-container">ARTICLES</div>
+      <div className="aricles-container">
+        <div className="breath-card">
+          <div className="card-1">
+            Anxiety <br></br> <div></div>
+          </div>
 
+          <div className="card-2">
+            <img src={anxiety} className="mini-luna" />
+          </div>
+        </div>
+
+        <div className="breath-card">
+          <div className="card-1">
+            Depression <br></br>
+          </div>
+
+          <div className="card-2">
+            <img src={depression} className="mini-luna" />
+          </div>
+        </div>
+
+        <div className="breath-card">
+          <div className="card-1">Love & Romance </div>
+
+          <div className="card-2">
+            <img src={love} className="mini-luna" />
+          </div>
+        </div>
+
+        <div className="breath-card">
+          <div className="card-1">
+            Physical Welbeing <br></br>
+          </div>
+
+          <div className="card-2">
+            <img src={running} className="mini-luna" />
+          </div>
+        </div>
+
+        <div className="breath-card">
+          <div className="card-1">
+            Mindfulness <br></br>
+          </div>
+
+          <div className="card-2">
+            <img src={mindfulness} className="mini-luna" />
+          </div>
+        </div>
+        <div className="breath-card">
+          <div className="card-1">
+            Recovery <br></br>
+          </div>
+
+          <div className="card-2">
+            <img src={recovery} className="mini-luna" />
+          </div>
+        </div>
+      </div>
+
+      <div className="articles-3"></div>
       <Footer />
     </div>
   );
