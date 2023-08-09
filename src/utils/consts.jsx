d18074b78eeb6343cc97a81434264342b22873e3
Andy Cockroft <acockroft1994@gmail.com>
2023-08-09 15:14:28 +0200

    consts file fixed

--- src/utils/consts.jsx
+++ src/utils/consts.jsx
@@ -1,6 +1,6 @@
-export const ACCESS_TOKEN = "access_token";
+export const ACCESS_TOKEN = "authToken";
 
-const URL = process.env.REACT_APP_SERVER_URL;
+const URL = import.meta.env.REACT_APP_SERVER_URL;
 export const SERVER_URL = URL ? `{URL}` : `http://localhost:5005`;
 
 export const USER_ROLE = ["User", "Therapist"];
